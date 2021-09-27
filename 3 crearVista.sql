71d093d545f877cd32015fe36b85a5c6dc684b0e
Cristian Ayala <cdap_13@hotmail.com>
2021-09-26 23:48:59 -0500

    Actualizados los scripts de acuerdo a los puntos solicitados

--- 3 crearVista.sql
+++
@@ -1,1 +0,0 @@
-CREATE VIEW documentosPersona AS SELECT Documento.`abreviatura`, Persona.`nombres`,Persona.`apellidos` FROM Persona INNER JOIN Documento ON Documento.`idPersona` = Persona.`idPersona`
